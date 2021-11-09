bcbc50c3926149def376b21b1f1a540b195ac5d7
rakiyama <ryojiro25@gmail.com>
2021-11-09 21:10:56 +0900

    delete main file

--- test_main/strchr.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-	//strchr
-	printf("strchr-----------------------------------------\n");
-	printf("strchr:%s:%s:%s\n", strchr("akiyama", '\0'), strchr("akiyama", 'x'), strchr("akiyama", 'i'));
-	printf("ft_strchr:%s:%s:%s\n", ft_strchr("akiyama", '\0'), ft_strchr("akiyama", 'x'), ft_strchr("akiyama", 'i'));
-}
